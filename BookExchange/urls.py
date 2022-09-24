c8477d3b614c0d6ffc2c12cf7380b647b2807c3a
Aaron Parson <85695649+Aparson13@users.noreply.github.com>
2022-09-24 11:48:32 -0400

    Update urls.py

--- BookExchange/urls.py
+++ BookExchange/urls.py
@@ -3,8 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   # path('', views.index, name='index'),
-    path('', include('mysite.urls')),
-    path('mysite/', include('mysite.urls')),
-    path('admin/', admin.site.urls),
+   path('', views.index, name='index')
 ]
