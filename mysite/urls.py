877cd9a332c762a068181ba2b515494735c682f4
Aaron Parson <85695649+Aparson13@users.noreply.github.com>
2022-09-24 12:07:17 -0400

    Adding url Expressions
    Adding url Expressions

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('', include('BookExchange.urls')),
     path('home/', include('BookExchange.urls')),
     path('admin/', admin.site.urls),
 ]
