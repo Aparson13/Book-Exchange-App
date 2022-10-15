b0152d6749afa1483713f662fa6f9191e3d4d304
oemyb07 <112432338+oemyb07@users.noreply.github.com>
2022-10-15 12:04:24 -0400

    Revert "Made logout button Easier to see"
    This reverts commit fe1610dc647d5ce27b70817c82ed562ec4ab57da.

--- BookExchange/urls.py
+++ BookExchange/urls.py
@@ -2,11 +2,10 @@ from . import views
 from django.urls import path, include
 from django.views.generic import TemplateView
 from django.contrib.auth.views import LogoutView
-from django.contrib.auth.views import LoginView
+
 urlpatterns = [
    path('', TemplateView.as_view(template_name="index.html")),
    path('', views.index, name='index'), 
    path('accounts/', include('allauth.urls')),
-   path('login/', LoginView.as_view(template_name='users/login.html'), name="login"),
    path('logout', LogoutView.as_view()),
 ]
