d82a4cd06697eb62084a98db58295ebb5e821dd6
Paul Nyberg <111672140+PaulNyberg00@users.noreply.github.com>
2022-10-22 00:26:07 -0400

    price positive only

--- BookExchange/models.py
+++ BookExchange/models.py
@@ -1,12 +1,17 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator
+from django.core.exceptions import ValidationError
+from decimal import Decimal
 # Create your models here.
 
 class Textbooks(models.Model):
     name = models.CharField(max_length=255, null=False)
     author = models.CharField(max_length=255, null=False)
     condition = models.CharField(max_length=255, null=False)
-    price = models.DecimalField(max_digits=10, decimal_places=2, default = 0)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default = 0, validators=[MinValueValidator(Decimal(0.00))]) 
+    def negCheck(price):
+        if (price < 0):
+            raise ValidationError('Cannot enter a negative value.')
     def __str__(self):
         return "{} - {} - {}".format(self.name, self.author, self.condition, self.price)
 
