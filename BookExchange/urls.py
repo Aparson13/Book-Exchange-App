b0ecc8088af113a4d9d5a9a0bac0059a2990c119
Aparson13 <aparson1700@gmail.com>
2022-10-22 19:45:57 -0400

    Readded urls

--- BookExchange/urls.py
+++ BookExchange/urls.py
@@ -7,5 +7,11 @@ urlpatterns = [
    path('', TemplateView.as_view(template_name="index.html")),
    path('', views.index, name='index'), 
    path('accounts/', include('allauth.urls')),
+   path('profile', views.Profilesv, name='profile'),
+   #path('login', auth_view.LoginView.as_view(), name='login'),
    path('logout', LogoutView.as_view()),
+   path('textbooks/', ListTextbooksView.as_view(), name="textbooks-all"),
+   path('write/', SellTextbooksWrite, name="write-textbooks"),
+   path('sell/', SellTextbooksView.as_view(), name="sell-textbooks"),
+   path('professors/', views.professors, name = 'professors')
 ]
