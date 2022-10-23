2025f9db6efda64952f38d80dc075e17702f7361
Paul Nyberg <111672140+PaulNyberg00@users.noreply.github.com>
2022-10-23 00:56:46 -0400

    adjusted user, added test case

--- BookExchange/models.py
+++ BookExchange/models.py
@@ -18,7 +18,7 @@ from django.utils import timezone
 
 class UserManager(BaseUserManager):
 
-  def _create_user(self, email, password, is_staff, is_superuser, **extra_fields):
+  def _create_user(self, username, email, password, is_staff, is_superuser, **extra_fields):
     if not email:
         raise ValueError('Users must have an email address')
     now = timezone.now()
@@ -36,16 +36,17 @@ class UserManager(BaseUserManager):
     user.save(using=self._db)
     return user
 
-  def create_user(self, email, password, **extra_fields):
-    return self._create_user(email, password, False, False, **extra_fields)
+  def create_user(self,username, email, password, **extra_fields):
+    return self._create_user(username, email, password, False, False, **extra_fields)
 
-  def create_superuser(self, email, password, **extra_fields):
-    user=self._create_user(email, password, True, True, **extra_fields)
+  def create_superuser(self, username, email, password, **extra_fields):
+    user=self._create_user(username, email, password, True, True, **extra_fields)
     user.save(using=self._db)
     return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=254, unique=True)
     email = models.EmailField(max_length=254, unique=True)
     name = models.CharField(max_length=254, null=True, blank=True)
     is_staff = models.BooleanField(default=False)
@@ -55,9 +56,9 @@ class User(AbstractBaseUser, PermissionsMixin):
     date_joined = models.DateTimeField(auto_now_add=True)
     
 
-    USERNAME_FIELD = 'email'
+    USERNAME_FIELD = 'username'
     EMAIL_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = [ 'email']
 
     objects = UserManager()
 
--- BookExchange/tests.py
+++ BookExchange/tests.py
@@ -1,11 +1,9 @@
-
 from django.test import TestCase
 from django.urls import reverse
 from rest_framework.test import APITestCase, APIClient
 from rest_framework.views import status
-from .models import Textbooks
+from .models import Textbooks, User
 from .serializers import TextbooksSerializer
-from django.core.exceptions import ValidationError
 
 class FirstTest(TestCase):
     def testFour(self):
@@ -38,15 +36,22 @@ class GetAllTextbooksTest(BaseViewTest):
         self.assertEqual(response.data, serialized.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
+
 class NegativePriceTest(TestCase):
     client = APIClient()
     @staticmethod
     def add_textbook(name="", author="", condition="", price=0):
         if name!= "" and author!= "" and condition!= "" and price!=-1:
             Textbooks.objects.create(name=name, author=author, condition=condition, price=price)
-            
+           
     def test_neg_price(self):
         with self.assertRaises(Exception):
             illegal = Textbooks.objects.create(name='chem', author='joe', condition='old', price=-1)
             illegal.negCheck()
-    
+
+class UserTest(TestCase):
+    def test_User(self):
+        self.user = User.objects.create_user(username='paul', email='testuser@gmail.com', password='12345678')
+        self.client.login(username='paul', email='testuser@gmail.com', password='12345678')
+        TRUE = 1
+        self.assertTrue(TRUE)
